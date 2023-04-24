4200a7715df29ab026abe529cbdab288becfc0d9
Baranov Nikita <nbaranov.vrn@gmail.com>
2023-04-24 18:10:22 +0300

    add readme and fix output blocks

--- output.py
+++ output.py
@@ -1,10 +1,10 @@
 def log_commission(ip, header, result, output):
     with open(f"log/{ip}.md", "a", encoding="UTF-8") as md:
         if result == "OK":
-            md.write(f'## * <span style="color:green"> {header} {result} </span> * \n\n')
+            md.write(f'## <span style="color:green"> {header} {result} </span>\n\n')
         else:
-            md.write(f'## * <span style="color:red"> {header} {result} </span> *\n\n')
-        md.write(f" ' {output} ' \n\n")
+            md.write(f'## <span style="color:red"> {header} {result} </span>\n\n')
+        md.write(f" ```\n {output} \n``` \n\n")
         
     with open(f"log/{ip}.txt", "a", encoding="UTF-8") as txt:
         txt.write(f"\n\n {header} --- {result} \n\n {output}")
---
+++ readme.md
@@ -0,0 +1,11 @@
+## Before start install poetry
+`pip install poetry`
+
+## Preparing venv
+`poetry install`
+`poetry shell`
+
+## For run test
+`python acc/main.py`
+or
+`poetry run python acc/main.py`
