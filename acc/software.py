2b0c5ccc472c001900fe053144e8a16bc8f4b894
Baranov Nikita <nbaranov.vrn@gmail.com>
2023-05-02 17:54:23 +0300

    acc/software.py add parsing for def display_version

--- acc/software.py
+++ acc/software.py
@@ -1,21 +1,29 @@
-# Software (display version, display startup, display patch-information)
+# Software 
+# display version, 
+# display startup, 
+# display patch-information
+
 from pprint import pprint
     
 
 def display_version(acc_ne):
     command = "display version"
+    clear_output = '' 
     output = acc_ne.send_command(command)
-    # TODO parsing and checking
-    result = "FAIL" #for test
-    return command, result, output
+    for line in output.split("\n"):
+        if "software" in line or "uptime" in line:
+            clear_output += f"{line} \n"
+    result = "OK" if clear_output else "FAIL"
+    return command, result, clear_output
 
-    
+
 def display_startup(acc_ne):
     command = "display startup"
     output = acc_ne.send_command(command)
     # TODO parsing and checking
     result = "OK" #for test
     return command, result, output    
+
 
 def display_patch_information(acc_ne):
     command = "display patch-information"
