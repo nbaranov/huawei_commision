8a445a69491ccdfdb72fc501b48bd5e152337ff0
Baranov Nikita <nbaranov.vrn@gmail.com>
2023-06-02 16:56:43 +0300

    created models for devices, categoryes and commands

--- commision/admin.py
+++ commision/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Command, Category, Device 
+
 # Register your models here.
+admin.site.register(Command)
+admin.site.register(Category)
+admin.site.register(Device)
