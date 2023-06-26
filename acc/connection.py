91a49fcda3e948c0a193994ff4359083e9b763b4
Baranov Nikita <nbaranov.vrn@gmail.com>
2023-06-26 18:06:17 +0300

    add command to DB, some fix for autocheking

--- acc/connection.py
+++ acc/connection.py
@@ -2,7 +2,6 @@ from netmiko.huawei import HuaweiSSH
 from netmiko.exceptions import NetmikoAuthenticationException
 from netmiko.exceptions import SSHException
 from netmiko.exceptions import NetmikoTimeoutException
-from asgiref.sync import sync_to_async
 
 
 def run_command_list(ip, username, password, com_list):
@@ -15,11 +14,11 @@ def run_command_list(ip, username, password, com_list):
                 output = ne.send_command(com.command)
                 check_status = ''
                 if com.check_include:
-                    for string in com.check_include.split(';;'):
-                        check_status = 'ok' if string.lower() in output.lower() else check_status
+                    for string in com.check_include.split('\n'):
+                        check_status = 'ok' if string in output else check_status
                 if com.check_exclude:
-                    for string in com.check_exclude.split(';;'):
-                        check_status = 'false' if string.lower() in output.lower() else check_status
+                    for string in com.check_exclude.split('\n'):
+                        check_status = 'false' if string in output else check_status
                 if com.out_line_limit:
                     output = '\n'.join(output.split('\n')[:com.out_line_limit])
                 yield { 
